c5504370b97ba18a67d53cbb3f3ef862841d4a99
skoblins <adam.skoblinski@nsn.com>
2013-11-27 23:56:15 +0100

    refactoring

--- configuration.sh
+++ configuration.sh
@@ -1,10 +1,12 @@
-CMAKE_OUTPUT_DIR=/var/tmp/wunderwaffe
-CMAKE_SOURCE_DIR=~/workspace/wunderwaffe-c++
+PROJECT_NAME=wunderwaffe-c++
+
+CMAKE_OUTPUT_DIR=/var/tmp/$PROJECT_NAME
+CMAKE_SOURCE_DIR=~/workspace/$PROJECT_NAME
 
 TOOLCHAIN_FILES="clang.cmake"
 TOOLCHAIN_FILES+=" amd64.cmake"
 TOOLCHAIN_FILES+=" ppc.cmake"
-# TOOLCHAIN_FILES+=" mips64.cmake"
+TOOLCHAIN_FILES+=" mips64.cmake"
 # TOOLCHAIN_FILES+=" mips64-octeon.cmake"
 TOOLCHAIN_FILES+=" arm.cmake"
 TOOLCHAIN_FILES+=" mingw-gcc-amd64.cmake"

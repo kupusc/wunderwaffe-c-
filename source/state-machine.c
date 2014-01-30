91273fc26dd2185b675c0b9490869e34ed31c358
skoblins <adam.skoblinski@nsn.com>
2014-01-30 19:00:04 +0100

    add basic functionality to state-machine

--- source/state-machine.c
+++ source/state-machine.c
@@ -1,6 +1,16 @@
 #include "state-machine.h"
 
-void process()
+handler_t* handlers[100][100];
+
+int current_state = 0;
+
+void install_handler(int state, int interrogation, handler_t* handler)
 {
-    
+    handlers[state][interrogation] = handler;
+}
+
+void handle(int interrogation)
+{
+    handler_t* func = handlers[current_state][interrogation];
+    current_state = (*func)();
 }

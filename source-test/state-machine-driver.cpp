fd191eb6c306fb5848aef9c62451ff08f11a5d80
skoblins <adam.skoblinski@nsn.com>
2014-02-05 22:07:52 +0100

    change name

--- source-test/state-machine-driver.cpp
+++ source-test/state-machine-driver.cpp
@@ -24,11 +24,11 @@ int test_handler1()
 TEST_GROUP(state_machine_driver_must)
 {
     state_machine_driver_handler_t *handlers[8];
-    state_machine_data object;
+    state_machine_data data;
     void setup()
     {
-        object.handlers = handlers;
-        state_machine_driver_init(&object);
+        data.handlers = handlers;
+        state_machine_driver_init(&data);
     }
 };
 
@@ -37,31 +37,31 @@ TEST_GROUP(state_machine_driver_must)
 
 TEST(state_machine_driver_must, link)
 {
-    state_machine_driver_install_handler(0, 0, 0, &object);
+    state_machine_driver_install_handler(0, 0, 0, &data);
 }
 
 
 TEST(state_machine_driver_must, handle_interrogation_in_initial_state_and_return_the_next_state_returned_by_handler)
 {
-    const int state = object.current_state;
+    const int state = data.current_state;
     const int interrogation = 2;
-    state_machine_driver_install_handler(state, interrogation, test_handler0, &object);
-    CHECK_EQUAL(test_handler0(), state_machine_driver_handle(interrogation, &object));
+    state_machine_driver_install_handler(state, interrogation, test_handler0, &data);
+    CHECK_EQUAL(test_handler0(), state_machine_driver_handle(interrogation, &data));
 }
 
 TEST(state_machine_driver_must, keep_internal_state)
 {
-    const int initial_state = object.current_state;
+    const int initial_state = data.current_state;
     const int interrogation0 = 0;
     const int interrogation1 = 1;
     const int state0 = test_handler0();
     const int state1 = test_handler1();
 
-    state_machine_driver_install_handler(initial_state, interrogation0, test_handler0, &object);
-    state_machine_driver_install_handler(state0, interrogation1, test_handler1, &object);
+    state_machine_driver_install_handler(initial_state, interrogation0, test_handler0, &data);
+    state_machine_driver_install_handler(state0, interrogation1, test_handler1, &data);
 
-    CHECK_EQUAL(state0, state_machine_driver_handle(interrogation0, &object));
-    CHECK_EQUAL(state1, state_machine_driver_handle(interrogation1, &object));
+    CHECK_EQUAL(state0, state_machine_driver_handle(interrogation0, &data));
+    CHECK_EQUAL(state1, state_machine_driver_handle(interrogation1, &data));
 }
 
 TEST(state_machine_driver_must, be_immune_to_race_conditions)
--- source/state-machine-driver.c
+++ source/state-machine-driver.c
@@ -3,20 +3,20 @@
 void state_machine_driver_install_handler(int state,
         int interrogation,
         state_machine_driver_handler_t *handler,
-        struct state_machine_data *object)
+        struct state_machine_data *data)
 {
-    object->handlers[state*interrogation] = handler;
+    data->handlers[state*interrogation] = handler;
 }
 
 int state_machine_driver_handle(int interrogation,
-                                struct state_machine_data *object)
+                                struct state_machine_data *data)
 {
-    state_machine_driver_handler_t *func = object->handlers[object->current_state * interrogation];
-    object->current_state = (*func)();
-    return object->current_state;
+    state_machine_driver_handler_t *func = data->handlers[data->current_state * interrogation];
+    data->current_state = (*func)();
+    return data->current_state;
 }
 
-void state_machine_driver_init(struct state_machine_data *object)
+void state_machine_driver_init(struct state_machine_data *data)
 {
-    object->current_state = 0;
+    data->current_state = 0;
 }

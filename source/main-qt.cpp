c3645873ec1879970956c9bd303ec6ee7bbcbb53
skoblins <adam.skoblinski@nsn.com>
2015-10-23 00:07:20 +0200

    add qt main file, add comments in cmake

---
+++ source/main-qt.cpp
@@ -0,0 +1,10 @@
+#include <QtGui/QApplication>
+#include <QtGui/QPushButton>
+
+int main(int argc, char *argv[])
+{
+    QApplication application(argc, argv);
+    QPushButton  button("Hello world!");
+    button.show();
+    return application.exec();
+}   

93478974783b4262b0cdc41d09f7ef119bce367c
skoblins <adam.skoblinski@nsn.com>
2016-03-30 13:27:25 +0200

    start writing my llvm pass

---
+++ source-test/llvm-pass.cpp
@@ -0,0 +1,21 @@
+#include <CppUTest/TestHarness.h>
+
+//////////  Stubs /////////
+
+
+//////// Fixture //////////
+
+TEST_GROUP(llvm)
+{
+    void setup()
+    {
+    }
+};
+
+
+//////// Tests /////////////
+
+TEST(llvm, works)
+{
+
+}
---
+++ source/llvm-pass.cpp
@@ -0,0 +1,3 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"

d71df403857c66ed7a7dca17f9b3a9391f2ccb3c
skoblins <adam.skoblinski@nokia.com>
2016-11-07 00:21:11 +0100

    add build stub test

---
+++ source-test/build.cpp
@@ -0,0 +1,23 @@
+#include <CppUTest/TestHarness.h>
+
+extern "C"
+{
+    #include "build.h"
+}
+
+TEST_GROUP(builder_test)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+
+TEST(builder_test, does_nothing)
+{
+
+}
